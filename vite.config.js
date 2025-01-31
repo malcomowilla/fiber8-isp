ec229371782dc722b5b2e84df189c3ce8cd6155a
malcomowilla <owillamalcom@gmail.com>
2025-01-31 10:04:08 +0300

    proxy requests2

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,22 @@ export default defineConfig({
       '/api': {
         // target: 'https://38d6-102-221-35-116.ngrok-free.app',
         // target: 'http://localhost:4000',
-        target: 'https://fiber8.aitechs.co.ke',
-        // target: 'http://0.0.0.0:3000',
+        // target: 'https://fiber8.aitechs.co.ke',
+        // // target: 'http://0.0.0.0:3000',
+        // changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+
+
+
+        target: (req) => {
+          const host = req.headers.host; // Get the request hostname
+      
+          // Check if the host is "aitechs.co.ke" or any subdomain of it
+          if (host === 'aitechs.co.ke' || host.endsWith('.aitechs.co.ke')) {
+            return `https://${host}`; // Proxy dynamically based on the request domain
+          }
+          return 'http://0.0.0.0:3000'; // Default target if not matching
+        },
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
