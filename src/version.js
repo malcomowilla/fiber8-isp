94975bf03fb8a0e397d36ce4ca1ab3af988b2e56
malcomowilla <owillamalcom@gmail.com>
2025-06-17 18:44:49 +0300

    0.0.21

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.0.20';
+export const APP_VERSION = '0.0.21';
 export const APP_NAME = 'AITechs';
 export const APP_DESCRIPTION = 'Display, Designed, and Developed by AITechs';
