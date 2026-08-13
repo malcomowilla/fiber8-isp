d9239c373cdcfbd004376a76fb68d42af0ac59a4
malcomowilla <owillamalcom@gmail.com>
2026-08-13 12:46:42 +0300

    Private Network

--- src/network/PrivateNetwork.jsx
+++ src/network/PrivateNetwork.jsx
@@ -510,20 +510,20 @@ setLoadButton(false)
       </Typography>
 
       <ThemeProvider theme={tableTheme}>
+<div style={{ overflowX: 'auto', width: '100%' }}>
 
- <div style={{ overflowX: 'auto', width: '100%' }}>
       <MaterialTable
         title=""
         columns={[
           { title: <p className='text-sm text-black'>VPN Client Ip</p>, field: 'allowed_ips' },
           { title: <p className='text-sm text-black'>Date Created</p>, field: 'created_at' },
 
-        {
+          {
   title: <p className='text-sm text-black'>Connected Subnets</p>,
   field: 'private_ip',
   cellStyle: { maxWidth: 220, whiteSpace: 'pre-wrap', wordBreak: 'break-word' },
   headerStyle: { maxWidth: 220 }
-}
+},
 
           { title: <p className='text-sm text-black'>Status</p>, field: 'status' },
         ]}
@@ -615,8 +615,7 @@ options={{
       }),
     }}
       />
-
-</div>
+ </div>
 </ThemeProvider>
 
 
